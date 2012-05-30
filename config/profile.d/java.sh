ff72503516f93e9d7d2c178560cfbb4ee047e64c
EC2 Default User <ec2-user@ip-10-83-61-96.ec2.internal>
2012-05-30 09:13:36 +0000

    adding deploy config

---
+++ config/profile.d/java.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME="/usr/java/jdk1.7.0_04"
+export MAVEN_HOME="/opt/apache-maven-3.0.4"
+export PLAY_HOME="/opt/play-1.2.4"
+
+export PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PLAY_HOME:$PATH"
+
